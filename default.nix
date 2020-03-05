bcd5176e274c4c945257e86fd46893579255f520
makefu <github@syntax-fehler.de>
2020-03-05 22:31:11 +0100

    default.nix: pin package again

--- default.nix
+++ default.nix
@@ -1,5 +1,18 @@
-with import <nixpkgs> {};
-with python3.pkgs;
+let
+  # Look here for information about how to generate `nixpkgs-version.json`.
+  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+  pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
+  pinnedPkgs = import (builtins.fetchGit {
+    inherit (pinnedVersion) url rev;
+
+    ref = "nixos-unstable";
+  }) {};
+in
+
+# This allows overriding pkgs by passing `--arg pkgs ...`
+{ pkgs ? pinnedPkgs }:
+
+with pkgs.python3.pkgs;
 buildPythonPackage {
   name = "env";
   src = ./.;
@@ -8,5 +21,5 @@ buildPythonPackage {
     requests
     beautifulsoup4
   ];
-  checkInputs = [ python3.pkgs.black ];
+  checkInputs = [ black ];
 }
