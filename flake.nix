4cb081131a8bfbff25fbef8778d95db047c72770
Bryan Lai <bryanlais@gmail.com>
2024-02-02 18:04:41 +0800

    Bump python 3.10 -> 3.11 and nixpkgs
    - bump nixpkgs, use python 3.11
    - poetry2nix now out of tree, import as flake input
    - add minor cleanup of flake.nix
    - update poetry dependencies

--- flake.nix
+++ flake.nix
@@ -3,41 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.systems.follows = "flake-utils/systems";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ ];
         };
-        python = pkgs.python310;
+        poetry2nix = self.inputs.poetry2nix.lib.mkPoetry2Nix {
+          inherit pkgs;
+        };
+        python = pkgs.python311;
         packageName = "hydra-check";
         # update the pyproject.toml too
         packageVersion = "1.3.5";
       in
       {
-        packages = rec {
-          hydra-check = python.pkgs.buildPythonApplication rec {
+        packages = {
+          hydra-check = python.pkgs.buildPythonApplication {
             pname = packageName;
             version = packageVersion;
             format = "pyproject";
             nativeBuildInputs = with python.pkgs; [ poetry-core ];
             propagatedBuildInputs = with python.pkgs; [ requests beautifulsoup4 colorama ];
             src = ./.;
-            checkInputs = with pkgs; [ python310.pkgs.mypy ];
+            checkInputs = with python.pkgs; [ mypy ];
             checkPhase = ''
               #export MYPYPATH=$PWD/src
               #mypy --strict .
             '';
           };
-          default = hydra-check;
+          default = self.packages.${system}.hydra-check;
         };
 
         devShells = {
@@ -45,10 +54,10 @@
             buildInputs = with pkgs; [
               pyright
               poetry
-              (pkgs.poetry2nix.mkPoetryEnv {
+              (poetry2nix.mkPoetryEnv {
                 inherit python;
                 projectDir = ./.;
-                overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: { });
+                overrides = poetry2nix.overrides.withDefaults (self: super: { });
                 editablePackageSources = {
                   hydra-check = ./src;
                 };
