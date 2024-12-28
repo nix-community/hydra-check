fc2389ff9663af5f9cce183410ca204f0cd155ca
Bryan Lai <bryanlais@gmail.com>
2024-12-28 14:29:32 +0800

    feat: reuse packaging from nixpkgs

--- package.nix
+++ package.nix
@@ -1,60 +1,44 @@
 {
   lib,
+  hydra-check,
   rustPlatform,
-  pkg-config,
-  openssl,
-  stdenv,
-  installShellFiles,
   versionCheckHook,
 }:
 
-rustPlatform.buildRustPackage {
-  pname = "hydra-check";
-  version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+hydra-check.overrideAttrs (
+  {
+    meta ? { },
+    nativeInstallCheckInputs ? [ ],
+    ...
+  }:
+  {
+    version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
 
-  src = builtins.path {
-    name = "hydra-check-source";
-    path = ./.;
-  };
-
-  cargoLock = {
-    lockFile = builtins.path {
-      name = "hydra-check-Cargo.lock";
-      path = ./Cargo.lock;
+    # `builtins.path` works well with lazy trees
+    src = builtins.path {
+      name = "hydra-check-source";
+      path = ./.;
     };
-  };
 
-  nativeBuildInputs = [
-    pkg-config
-    installShellFiles
-  ];
+    cargoDeps = rustPlatform.importCargoLock {
+      lockFile = builtins.path {
+        name = "hydra-check-Cargo.lock";
+        path = ./Cargo.lock;
+      };
+    };
 
-  buildInputs = [
-    openssl
-  ];
-
-  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    installShellCompletion --cmd hydra-check \
-      --bash <($out/bin/hydra-check --shell-completion bash) \
-      --fish <($out/bin/hydra-check --shell-completion fish) \
-      --zsh <($out/bin/hydra-check --shell-completion zsh)
-  '';
-
-  nativeInstallCheckInputs = [
-    versionCheckHook
-  ];
-
-  doInstallCheck = true;
-
-  meta = {
-    description = "Check hydra for the build status of a package";
-    homepage = "https://github.com/nix-community/hydra-check";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [
-      makefu
-      artturin
-      bryango
+    nativeInstallCheckInputs = nativeInstallCheckInputs ++ [
+      versionCheckHook
     ];
-    mainProgram = "hydra-check";
-  };
-}
+
+    doInstallCheck = true;
+
+    meta = meta // {
+      maintainers = with lib.maintainers; [
+        makefu
+        artturin
+        bryango
+      ];
+    };
+  }
+)
