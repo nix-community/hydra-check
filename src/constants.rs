a2f24cfffe4131e8f591234364cd8d8f498c49ca
Bryan Lai <bryanlais@gmail.com>
2025-01-24 14:35:26 +0800

    feat(constants): add & document supported --arch
    Add new constant for supportedSystems for NixOS.

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,13 @@
 //! Useful constants shared across the program
 
-/// Currently supported systems on [hydra.nixos.org][hydra].
-/// This may change in the future.
+/// Currently supported systems (`supportedSystems`) on [hydra.nixos.org](https://hydra.nixos.org).
 ///
-/// [hydra]: https://hydra.nixos.org
+/// This is invoked in the jobset: [nixpkgs/trunk](https://hydra.nixos.org/jobset/nixpkgs/trunk#tabs-configuration),
+/// and defined by the following expressions in nixpkgs:
+/// - [pkgs/top-level/release.nix](https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/release.nix)
+/// - [ci/supportedSystems.nix](https://github.com/NixOS/nixpkgs/blob/master/ci/supportedSystems.nix).
+///
+/// This may change in the future.
 ///
 /// ```
 /// assert_eq!(hydra_check::constants::KNOWN_ARCHITECTURES, [
@@ -19,6 +23,25 @@ pub const KNOWN_ARCHITECTURES: [&str; 4] = [
     "aarch64-linux",
     "x86_64-darwin",
     "aarch64-darwin",
+];
+
+/// Currently supported systems (`supportedSystems`) for NixOS.
+///
+/// This is invoked in the jobset: [nixos/trunk-combined](https://hydra.nixos.org/jobset/nixos/trunk-combined#tabs-configuration),
+/// and defined by the expression: [nixpkgs: nixos/release-combined.nix](https://github.com/NixOS/nixpkgs/blob/master/nixos/release-combined.nix).
+///
+/// This may change in the future.
+///
+/// ```
+/// assert_eq!(hydra_check::constants::NIXOS_ARCHITECTURES, [
+///     "x86_64-linux",
+///     "aarch64-linux",
+/// ]);
+/// ```
+///
+pub const NIXOS_ARCHITECTURES: [&str; 2] = [
+    "x86_64-linux",
+    "aarch64-linux",
 ];
 
 /// Default package filter for the details of a specific evaluation.
