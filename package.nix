c75b0472d61dd952bb6b3d147e3f84552ac142c3
Bryan Lai <bryanlais@gmail.com>
2026-01-05 11:37:58 +0800

    fix(package.nix): correct versioning for nix build

--- package.nix
+++ package.nix
@@ -13,7 +13,7 @@
 
 let
 
-  packageVersion = with builtins; (fromTOML (readFile "${source}/Cargo.toml")).package.version;
+  packageVersion = with builtins; (fromTOML (readFile ./Cargo.toml)).package.version;
 
   # append git revision to the version string, if available
   versionSuffix =
@@ -33,35 +33,37 @@ hydra-check.overrideAttrs (
     meta ? { },
     ...
   }@_prevAttrs:
-  if isDevShell then
-    {
-      # prevent devShell dependence on the source
-      src = emptyDirectory;
-      cargoDeps = emptyDirectory;
-    }
-  else
-    {
-      version =
-        assert lib.assertMsg (lib.versionAtLeast newVersion version) ''
-          hydra-check provided here (${newVersion}) failed to be newer
-          than the one provided in nixpkgs (${version}).
-        '';
-        newVersion;
+  (
+    if isDevShell then
+      {
+        # prevent devShell dependence on the source
+        src = emptyDirectory;
+        cargoDeps = emptyDirectory;
+      }
+    else
+      {
+        src = source;
+        cargoDeps = rustPlatform.importCargoLock {
+          lockFile = "${source}/Cargo.lock";
+        };
+      }
+  )
+  // {
+    version =
+      assert lib.assertMsg (lib.versionAtLeast newVersion version) ''
+        hydra-check provided here (${newVersion}) failed to be newer
+        than the one provided in nixpkgs (${version}).
+      '';
+      newVersion;
 
-      src = source;
-
-      cargoDeps = rustPlatform.importCargoLock {
-        lockFile = "${source}/Cargo.lock";
-      };
-
-      meta = meta // {
-        maintainers = with lib.maintainers; [
-          makefu
-          artturin
-          bryango
-        ];
-        # to correctly generate meta.position for backtrace:
-        inherit (meta) description;
-      };
-    }
+    meta = meta // {
+      maintainers = with lib.maintainers; [
+        makefu
+        artturin
+        bryango
+      ];
+      # to correctly generate meta.position for backtrace:
+      inherit (meta) description;
+    };
+  }
 )
