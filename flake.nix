117d33eccd3b7a2ff8df5bfd8679d4f47a8aad12
Bryan Lai <bryanlais@gmail.com>
2026-01-16 22:47:27 +0800

    chore: reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -10,8 +10,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
@@ -21,24 +28,40 @@
           default = self.packages.${system}.hydra-check;
         };
 
-        devShells.default = (self.packages.${system}.hydra-check.override {
-          # prevent dependence on the source ./.; see ./package.nix
-          source = null;
-        }).overrideAttrs ({ nativeBuildInputs ? [], env ? {}, ... }: {
-          nativeBuildInputs = with pkgs.buildPackages; [
-            git
-            cargo # with shell completions, instead of cargo-auditable
-            cargo-insta # for updating insta snapshots
-            clippy # more lints for better rust code
-            nixfmt-rfc-style # for formatting nix code
-          ] ++ nativeBuildInputs;
+        devShells.default =
+          (self.packages.${system}.hydra-check.override {
+            # prevent dependence on the source ./.; see ./package.nix
+            source = null;
+          }).overrideAttrs
+            (
+              {
+                nativeBuildInputs ? [ ],
+                env ? { },
+                ...
+              }:
+              {
+                nativeBuildInputs =
+                  with pkgs.buildPackages;
+                  [
+                    git
+                    cargo # with shell completions, instead of cargo-auditable
+                    cargo-insta # for updating insta snapshots
+                    clippy # more lints for better rust code
+                    nixfmt-rfc-style # for formatting nix code
+                  ]
+                  ++ nativeBuildInputs;
 
-          env = with pkgs.buildPackages; (removeAttrs env ["RUST_LOG"]) // {
-            # for developments, e.g. symbol lookup in std library
-            RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
-            # for debugging
-            RUST_LIB_BACKTRACE = "1";
-          };
-        });
-      });
+                env =
+                  with pkgs.buildPackages;
+                  (removeAttrs env [ "RUST_LOG" ])
+                  // {
+                    # for developments, e.g. symbol lookup in std library
+                    RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+                    # for debugging
+                    RUST_LIB_BACKTRACE = "1";
+                  };
+              }
+            );
+      }
+    );
 }
